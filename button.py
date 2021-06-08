7eea2e262d9c65ea52a7c6829b0611f7559534ef
volodumir <volodumir@gmail.com>
2021-06-08 23:06:35 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,1 +1,16 @@
-print("Goodbye, World!")
+import keyboard
+
+translate = {
+    'down':' нажал на клавишу ',
+    'up':' отпустил клавишу '
+}
+
+
+def print_pressed_keys(e):
+    print(
+        'Пользователь {}{}'.format(translate[e.event_type], e.name)
+        )
+
+
+keyboard.hook(print_pressed_keys)
+keyboard.wait()
