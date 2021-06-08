4defb1e8372efd02c863f398fab02390b9ab3c69
volodumir <volodumir@gmail.com>
2021-06-09 00:00:51 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,16 +1,11 @@
-import keyboard
+import keyboard  # using module keyboard
 
-translate = {
-    'down':' нажал на клавишу ',
-    'up':' отпустил клавишу '
-}
-
-
-def print_pressed_keys(e):
-    print(
-        'Пользователь {}{}'.format(translate[e.event_type], e.name)
-        )
-
-
-keyboard.hook(print_pressed_keys)
-keyboard.wait()
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+            print('You Pressed A Key!')
+            break  # finishing the loop
+        else:
+            pass
+    except:
+        break  # if user pressed a key other than the given key the loop will break
